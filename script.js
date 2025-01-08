2d56842daa2fa4b653071b1585aeed041d84606b
JyotiradeetiBagal <139766154+JyotiradeetiBagal@users.noreply.github.com>
2025-01-08 22:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",()=>{
+	const message="DOM load success";
+	
+	const textNode=document.createTextNode(message);
+
+	document.body.appendChild(textNode);
+})
